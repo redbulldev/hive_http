2c3e6cade584dff7bfad22ea64d2d5f3b668a129
redbulldev <=nv.hung.red@gmail.com>
2022-11-10 17:22:58 +0700

    update from company

--- _temp/hr-tool-dev-me/crud/more/dashboard_cv.php
+++ _temp/hr-tool-dev-me/crud/more/dashboard_cv.php
@@ -1,6 +1,60 @@
 <?php
 use Illuminate\Database\Capsule\Manager as DB;
 
-// die('more_cv');
 
+// lọc data in table request //
 
+// die($params['department_id']);
+$obj->whereIn('request.status', ["*"])->where('request.isdelete',0);
+
+// die($params['department_id']);
+//start - chức năng "Bộ lọc thông tin:"
+if(!empty($params['department_id']))
+{
+    $idDepartment = explode('-',$params['department_id']);
+
+    $obj->orWhere(function($query) use ($idDepartment){
+        foreach ($idDepartment as $id) {
+            $query->orWhere('request.position_id' , 'LIKE', "%$id%");
+        }
+    });
+}
+
+// $data =[];
+// die($params['assignee_id']);
+if(!empty($params['assignee_id']))
+{
+    $idAssignee = explode('-',$params['assignee_id']);
+
+    $obj->orWhere(function($query) use ($idAssignee){
+        foreach ($idAssignee as $key  => $id) {
+        	// echo $id;
+            $query->orWhere('request.assignee_id' , 'LIKE', "%$id%");
+        }
+    });
+} 
+// // print_r($data);
+// die();
+if (!empty($params['from']) && !empty($params['to'])) {
+    $from = $params['from'];
+    $to = $params['to'];
+    $obj->where('date', '>=', $from)->where('date', '<=', $to);
+}
+
+// // map -> position_id
+// // die($params['position_id']);
+$obj->join('positions', function ($join) {
+    $join->on('positions.id', '=', 'request.position_id');
+    $join->where(['positions.status'=>1, 'positions.isdelete'=>0]);
+});
+
+// // map -> parent_id
+$obj->join('positions as parent', function ($join) {
+    $join->on('parent.id', '=', 'positions.parent_id');
+    $join->where(['parent.status' => 1, 'parent.isdelete' => 0]);
+});
+
+// thêm column vào struct data[]
+$moreselect= ['positions.title as positions_title', 'parent.title as department_title'];
+
+// die($response->withJson($obj->get()));
--- _temp/hr-tool-dev-me/crud/more/dashboard_cv_after.php
+++ _temp/hr-tool-dev-me/crud/more/dashboard_cv_after.php
@@ -7,38 +7,40 @@ $obj = DB::table($name);
 require './crud/all_where.php';   
 
 
-$objRequest = DB::table('request');
+// $objRequest = DB::table('request');
 // data summary
-// $obj->selectRaw('
-//     SUM(step) AS target, 
-//     SUM(total_cv) AS total_cv, 
-//     SUM(interview_cv) AS interview_cv, 
-//     SUM(pass_cv) AS pass_cv, 
-//     SUM(offer_cv) AS offer_cv, 
-//     SUM(offer_success) AS offer_success, 
-//     SUM(onboard_cv) AS onboard_cv, 
-//     SUM(fail_job) AS fail_job,
-//     GROUP_CONCAT(target) as list_target, 
-//     GROUP_CONCAT(total_cv) as list_total, 
-//     GROUP_CONCAT(onboard_cv) as list_onboard,
-//     GROUP_CONCAT(positions.title,\'\') as labels
-// ');
+$obj->selectRaw('
+    SUM(target) AS target, 
+    SUM(total_cv) AS total_cv, 
+    SUM(interview_cv) AS interview_cv, 
+    SUM(pass_cv) AS pass_cv, 
+    SUM(offer_cv) AS offer_cv, 
+    SUM(offer_success) AS offer_success, 
+    SUM(onboard_cv) AS onboard_cv, 
+    SUM(fail_job) AS fail_job,
+    GROUP_CONCAT(target) as list_target, 
+    GROUP_CONCAT(total_cv) as list_total, 
+    GROUP_CONCAT(onboard_cv) as list_onboard,
+    GROUP_CONCAT(positions.title,\'\') as labels
+');
 // SUM(step) AS target, 
 
 // task - Đơn ứng tuyển/qua phỏng vấn:
-$test = $obj->where('step', 1)->where('status', 1)->selectRaw('
-    SUM(step) AS target, 
-    SUM(status) AS total_cv, 
-');
+// $test = $obj->selectRaw('
+//     SUM(target) AS target
+// ');
 
 // $countCv = $obj->where('step', 1)->count();
 
 // $countStep = $obj->count();
-echo "<pre>";
-print_r($test ) ;
-echo "</pre>";
+// echo "<pre>";
+// print_r($test ) ;
+// echo "</pre>";
 
-die();
+
+// die($response->withJson($obj->get()));
+
+// die('ok');
 
 
 // Số lượng cần tuyển:
@@ -70,43 +72,6 @@ die();
 
 
 
-//     1   id Primary  int         No  None    ID level;21 AUTO_INCREMENT  Change Change   Drop Drop   
-//     2   author_id Index varchar(150)    utf8mb4_unicode_ci      No  None    ID người tạo;namng      Change Change   Drop Drop   
-//     3   request_id Index    int         No  None    ID request;23       Change Change   Drop Drop   
-//     4   interviewer_id Index    varchar(150)    utf8mb4_unicode_ci      Yes NULL    Người phỏng vấn;huynhnv     Change Change   Drop Drop   
-//     5   reviewer_id Index   varchar(150)    utf8mb4_unicode_ci      Yes NULL    Người Review CV;thangpm     Change Change   Drop Drop   
-//     6   assignee_id varchar(150)    utf8mb4_unicode_ci      Yes NULL    Người xử lý;thanglv     Change Change   Drop Drop   
-//     7   chairman_id varchar(50) utf8mb4_unicode_ci      Yes lent    Người xem nhân tướng và pre offer;lent      Change Change   Drop Drop   
-//     8   position_id Index   int         Yes NULL    ID vị trí;123       Change Change   Drop Drop   
-//     9   level_id Index  int         Yes NULL    ID level;4      Change Change   Drop Drop   
-//     10  fullname    varchar(200)    utf8mb4_unicode_ci      No  None    Họ và tên; Nguyễn Xuân Khoát        Change Change   Drop Drop   
-//     11  email   varchar(250)    utf8mb4_unicode_ci      Yes NULL    Email;xuankhoat@gmail.com       Change Change   Drop Drop   
-//     12  mobile  varchar(15) utf8mb4_unicode_ci      Yes NULL    Số di động;09124578963      Change Change   Drop Drop   
-//     13  birthday    date            Yes NULL    Ngày sinh;1988-10-27        Change Change   Drop Drop   
-//     14  address varchar(250)    utf8mb4_unicode_ci      Yes NULL    Địa chỉ;123 Xuân Thủy, Cầu giấy, Hà Nội     Change Change   Drop Drop   
-//     15  gender  tinyint         Yes NULL    Giới tính:0 Nữ, 1 Nam;1     Change Change   Drop Drop   
-//     16  linkcv  varchar(250)    utf8mb4_unicode_ci      Yes NULL    Đường dẫn CV;https://download.com/cv.pdf        Change Change   Drop Drop   
-//     17  images  json            Yes NULL    Danh sách hình ảnh;["http://...jpg","http://...png"]        Change Change   Drop Drop   
-//     18  source_id   int         Yes NULL    Nguồn; TopCV        Change Change   Drop Drop   
-//     19  appoint_type    tinyint         Yes NULL    Loại hình phỏng vấn:0 Offline, 1 Online;0       Change Change   Drop Drop   
-//     20  description text    utf8mb4_unicode_ci      Yes NULL    Mô tả;Lorem ipsum dolor sit amet...     Change Change   Drop Drop   
-//     21  appoint_date    int         Yes NULL    Ngày phỏng vấn;1622504033       Change Change   Drop Drop   
-//     22  appoint_place   varchar(250)    utf8mb4_unicode_ci      Yes NULL    Nơi phỏng vấn;Phòng họp lớn     Change Change   Drop Drop   
-//     23  appoint_link    varchar(2100)   utf8mb4_unicode_ci      Yes NULL    Link phỏng vấn online;https://meet.ossigroup.net/interview      Change Change   Drop Drop   
-//     24  salary  int         Yes NULL    Lương vào làm (VND);40000000        Change Change   Drop Drop   
-//     25  checklist   varchar(2100)   utf8mb4_unicode_ci      Yes NULL    Link checklist;http://link.checklist.com        Change Change   Drop Drop   
-//     26  onboard date            Yes NULL    Ngày onboard;2022-05-15     Change Change   Drop Drop   
-//     27  datecreate  int         No  None    Ngày tạo;1622504033     Change Change   Drop Drop   
-//     28  datemodified    int         No  None    Ngày sửa cuối;1622505033        Change Change   Drop Drop   
-//     29  step    tinyint         No  0   Bước CV: 0: new, 1: Hr review,2: nhân tướng 1, 3: CV review,4: To interview,5: Interview,6: Nhân tướng 2,7: Pre offer,8: Offer,9: OnBoard,10: Thử việc;9        Change Change   Drop Drop   
-//     30  status  tinyint(1)          No  2   Trạng thái:0 Fail, 1 pedding, 2 Pass;0      Change Change   Drop Drop   
-//     31  isdelete    tinyint         No  0           Change Change   Drop Drop   
-
-
-
-
-
-
 
 
 // *Bảng xếp hạng số lượng đã tuyển //
@@ -115,21 +80,73 @@ $colors = ['#32E875','#FBB13C','#FF5D73','#8A84E2','#A3F4FF','#3495eb','#9e0211'
 
 $department = ['labels'=>[],'values'=>[],'colors'=>[]]; 
 
-$all = DB::table('positions')->where(['isdelete'=>0,'status'=>1,'parent_id'=>1])->get();
+$item = DB::table('positions')->where(['isdelete'=>0,'status'=>1, 'parent_id'=>0])->get(); 
 
-foreach($all as $key => $item)
-{
-    $obj1 = clone  $objRequest; //request
+$all = DB::table('positions')->where(['isdelete'=>0,'status'=>1])->get(); 
 
-    $res = $obj1->where('position_id',$item->id)->selectRaw('SUM(onboard_cv) AS total_onboard_cv')->first();
-    // print_r($res);
-
-    $department['labels'][] = $item->title;
-    $department['values'][] = $res->onboard_cv; //yêu cầu (số lượng)
-    $department['colors'][] = !empty($colors[$key]) ? $colors[$key]:'#000';
+// die($response->withJson($item));
+// get all position of phongban
+$data = [];
+$count = 0;
+foreach($item as $key => $value) {
+    foreach($all as $index => $l) {
+        if($l->parent_id == $value->id){
+            $department['labels'][] = $l->title;   
+            $data[$count] = $l->id; 
+            $count++;
+        }
+    }
 }
+// print_r($data);
+// die();
+// die( $response->withJson($data));
 
-print_r($department);
+
+$test=[];
+
+// $values = DB::table('request')->where(['isdelete'=>0])->get(); 
+// foreach($values as $key => $value) {
+    foreach ($data as $key  => $id) {
+        // if($id == $value->position_id){
+        //    $d = $value->author_id;
+        //    die($d);
+        // }
+        $value = DB::table('request')->where(['isdelete'=>0])->where('request.position_id' , $id)->selectRaw('SUM(target) AS target')->first();
+// die($value->target);
+        $test[$key]  = $value->target;
+    }
+// }
+die($response->withJson($test));
+
+// $obj1 = clone  $obj;
+// $obj1->where(function($query) use ($data){
+//     foreach ($data as $key  => $id) {
+//         // echo $id;
+//         $c = $query->where('request.position_id' , $id)->selectRaw('SUM(target) AS target')->first();
+// print_r($c->target);
+//         $test['values'][] = $c->target;
+//     }
+// });
+
+// print_r($department['values'][] );
+
+
+// $valueAndColor = DB::table('positions')->where(['isdelete'=>0,'status'=>1])->where('parent_id', '!=', 0)->get(); 
+
+// foreach($valueAndColor as $key => $k)
+// {
+//     $obj1 = clone  $obj; //request
+
+//     $res = $obj1->where('positions.parent_id',$k->id)->selectRaw('SUM(target) AS target')->first();
+//     // die($res->target);
+
+//     // $department['labels'][] = $res->title;
+
+//     $department['values'][] = $res->target; //yêu cầu (số lượng)
+//     $department['colors'][] = !empty($colors[$key]) ? $colors[$key]:'#000';
+// }
+die( $response->withJson($department));
+// print_r($department);
 die();
 
 
