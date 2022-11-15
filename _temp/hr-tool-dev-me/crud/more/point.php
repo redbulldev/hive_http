4ba03207c182735100bda4b08ee98ead45ed98e0
redbulldev <=nv.hung.red@gmail.com>
2022-11-15 17:24:32 +0700

    update 15/11

--- _temp/hr-tool-dev-me/crud/more/point.php
+++ _temp/hr-tool-dev-me/crud/more/point.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Capsule\Manager as DB;
 
-$obj->where(['point_status' => 1, 'isdelete' => 0, 'status' => 1])->where('parent_id', '!=', 0);
+// $obj->where(['point_status' => 1, 'isdelete' => 0, 'status' => 1])->where('parent_id', '!=', 0);
 
 
 // $levels = DB::table('level')->where(['status' => 1, 'isdelete' => 0]);
@@ -15,3 +15,56 @@ $obj->where(['point_status' => 1, 'isdelete' => 0, 'status' => 1])->where('paren
 //     'total' => $ketqua ? $ketqua->count() : null,
 //     'time' => time(),
 // ];
+
+
+
+// $obj->join('positions', function ($join) {
+//     $join->on('positions.id', '=', 'request.position_id');
+//     $join->where(['positions.status'=>1, 'positions.isdelete'=>0]);
+// });
+
+// $obj->join('level_positions', function ($join) {
+//     $join->on('positions.id', '=', 'level_positions.position_id');
+//     $join->where(['level_positions.isdelete'=>0]);
+//     $join->where(['positions.isdelete'=>0]); 
+// });
+// // die($response->withJson($obj->get()));
+
+// $obj->join('level', function ($join) {
+//     $join->on('level.id', '=', 'level_positions.level_id');
+//     $join->where(['level.isdelete'=>0]);
+// });
+
+// die($response->withJson($obj->get()));
+
+$getAll = $obj->join('level_positions', 'positions.id', '=', 'level_positions.position_id')->where(['level_positions.isdelete'=>0])
+            ->join('level', 'level.id', '=', 'level_positions.level_id')->where(['level.isdelete'=>0])
+            ->get(); 
+
+
+// $getAll = DB::table('level_positions as lp')->join('level', 'level.id', '=', 'lp.level_id')->where(['lp.isdelete'=>0])
+//             // ->join('level', 'level.id', '=', 'level_positions.level_id')->where(['level.isdelete'=>0])
+//             ->get(); 
+//             $dataid = [];
+// foreach($getAll as $key => $value){
+//     $dataid[$key] = $value->level_id;
+// }           
+die($response->withJson($getAll));
+
+
+
+//  $test = DB::table('level_positions as lp')->join('positions as p', 'p.id','=','lp.position_id')->get();
+// die($test);
+
+// die($getAll);
+// ＄users = DB::table('users')
+//             ->join('contacts', 'users.id', '=', 'contacts.user_id')
+//             ->join('orders', 'users.id', '=', 'orders.user_id')
+//             ->select('users.*', 'contacts.phone', 'orders.price')
+//             ->get();
+
+
+// cac buoc 
+// - diem get tu level_positions
+// kiem tra
+
--- _temp/hr-tool-dev-me/crud/more/point_after.php
+++ _temp/hr-tool-dev-me/crud/more/point_after.php
@@ -5,8 +5,8 @@ use Illuminate\Database\Capsule\Manager as DB;
 
 
 $getLevels = DB::table('level')->where(['status' => 1, 'isdelete' => 0])->get();
-
 // die($response->withJson($levels->get()));
+$level_positions = DB::table('level_positions as lp')->where('isdelete', 0)->get();
 
 
 $lables = [];
@@ -17,54 +17,116 @@ $values = [];
 
 $allPositions = $obj->get(); 
 
+
 foreach ($allPositions as $key => $value) {
 	$lables[$key] = $value->title;
+}
+
+function dataposition($value){
+// print_r($value);die();
+
+	$check = DB::table('positions')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->first();
+// die($check);
+	if($check){
+				// die('ok');
+
+		return $check->id;
+	}
+
+	return false;
+}
+// foreach ($allPositions as $key => $value) {
+// 	$lables[$key] = $value->title;
 	foreach ($getLevels as $index => $v) {
-		$levels[$index] = $v->title;
-		$values[$index] = $v->point;
+		$check = false;
+
+			 // print_r($level_positions[$3]->point);
+			 // die();
+		$levels[$v->id] = $v->title;
+		for($i=0;$i<count($level_positions);$i++) {
+			if ($level_positions[$i]->level_id == $v->id && $level_positions[$i]->position_id == dataposition($level_positions[$i]->position_id)) {//$value->id
+			 // print_r($level_positions[$i]->level_id);
+			 	$values[$level_positions[$i]->level_id ] = $level_positions[$i]->point;
+				// die('ok');
+				$check=true;
+
+
+				$data_summaries = [array_flip($lables)];
+				foreach($data_summaries as $test) {
+						foreach($test as $key => $value)
+						{
+							// print_r($value);die();
+							if($level_positions[$i]->position_id == $value){
+								$data_summaries[$key] = array($levels, $values);
+							}
+						}
+					}
+				}
+		
+		}
+		
+		if($check ==false){
+			$values[$v->id] = 1;
+				$data_summaries = [array_flip($lables)];
+
+			foreach($data_summaries as $test) {
+						foreach($test as $key => $value)
+						{
+							// print_r($value);die();
+							// if($level_positions[$i]->position_id == $value){
+								$data_summaries[$key] = array($levels, $values);
+							// }
+						}
+					}
+			}
 	}
-}
-
-// $summaries = array(
-// 		'positions' => array($lables),
-// 		'levels' => array($levels),
-// 		'values' => array($values)
-
-// );
-
-// $positionTitles = array(
-// 		'positions' => array($lables),
-// );
-
-$data_summaries = [array_flip($lables)];
-
-foreach($data_summaries as $test) {
-	foreach($test as $key => $value)
-	{
-		$data_summaries[$key] = array($levels, $values);
-	}
-}
-
-// die($response->withJson($data_summaries));
-
-
-// foreach ($summaries['lables']['position'] as $value) {
-// 	echo $value;
 // }
+// die();
 
-// foreach($summaries as $book) {
-// 	echo "<br>";
-// 	foreach($book['positions'] as $key => $value)
+// $data_summaries = [array_flip($lables)];
+
+// foreach($data_summaries as $test) {
+// 	foreach($test as $key => $value)
 // 	{
-// 		echo $value. "<br>";
+// 		$data_summaries[$key] = array($levels, $values);
 // 	}
 // }
-// die();
-// die($response->withJson($summaries));
 
-// echo count($levels);
-// die();
+// $data_summaries = [array_flip($lables)];
 
+// foreach($data_summaries as $test) {
+// 	foreach($test as $key => $v)
+// 	{
+// 		// $data_summaries[$key] = array($levels, $values);
+// 		foreach ($levels as $index => $l) {
+// 			$check = false;
+// 			foreach ($values as $i => $k) {
+// 				if($index == $i){
+// 					$values[$index] == $k;
+// 					$check=true;
+// 				}
+// 			}
+// 			if($check ==false){
+// 				$values[$index] = 1;
+// 			}
+// 		}
+// 		// $data_summaries[$key] = array($levels, $values);
+
+// 	}
+// }
+
+
+
+
+// $data_summaries = [array_flip($lables)];
+
+// foreach($data_summaries as $test) {
+// 	foreach($test as $key => $value)
+// 	{
+// 		$data_summaries[$key] = array($levels, $values);
+// 	}
+// }
+die($response->withJson($data_summaries));
 
 
 $results = [
