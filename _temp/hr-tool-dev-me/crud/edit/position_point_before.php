310c261627f4bcd8b75d34e88ce2c6583761f413
mousephp <nvhung710@gmail.com>
2022-11-23 07:14:47 +0700

    update from local 23/11

--- _temp/hr-tool-dev-me/crud/edit/position_point_before.php
+++ _temp/hr-tool-dev-me/crud/edit/position_point_before.php
@@ -3,26 +3,24 @@
 use Illuminate\Database\Capsule\Manager as DB;
 use Respect\Validation\Validator as v;
 
-
-// $name='level_positions';
-$name='positions';
-throwError($container,$request, [
-    'title' => v::length(2, 200)->notEmpty()
-]);
-echo 'ok';
-
-// throwError($container, $request, [
-//     'point' => v::length(1, 6)->notEmpty()->noWhitespace()
+// // $name='level_positions';
+// $name='positions';
+// throwError($container,$request, [
+//     'title' => v::length(2, 200)->notEmpty()
 // ]);
+// echo 'ok';
 
-die($data->point);
+// // throwError($container, $request, [
+// //     'point' => v::length(1, 6)->notEmpty()->noWhitespace()
+// // ]);
 
-// if (!empty($data->point)) {
-//     if (!preg_match("?<=^| )\d+(\.\d+)?(?=$|", $data->point)) {
-//         throw new Exception('Enter only numbers and periods (.)');
-//     }
-// }
+// die($data->point);
 
+// // if (!empty($data->point)) {
+// //     if (!preg_match("?<=^| )\d+(\.\d+)?(?=$|", $data->point)) {
+// //         throw new Exception('Enter only numbers and periods (.)');
+// //     }
+// // }
 
 // Giá trị mặc định bằng 1
 // Hệ thống hiển thị input box để chỉnh sửa
@@ -34,97 +32,138 @@ die($data->point);
 // - trim() khoảng trắng
 // Nút xác nhận chỉ Lưu khi có chỉnh sửa
 
-
-
-
-
-
-
-
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
+if (is_string($data->point)) {
+    throw new Exception('Invalid data!');
+}
 
-// $get_levels = DB::table('level')->where(['status' => 1, 'isdelete' => 0])->get();
+if (is_int($data->point)) {
+    if ($data->point < 1) {
+        throw new Exception('Invalid data!');
+    }
 
-// $level_positions = DB::table('level_positions as lp')->where(['isdelete' => 0])->where('position_id', '!=', 0)->get();
+    throwError($container, $request, [
+        'point' => v::digit()->length(1, 6)->notEmpty()->noWhitespace(),
+    ]);
+}
 
-// $levels = [];
+if (is_float($data->point)) {
+    $numlength = strlen((string) $data->point);
 
-// function checkPosition($value)
-// {
-//     $check = DB::table('positions')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->where('parent_id', '!=', 0)->first();
+    if ($numlength < 0 && $numlength > 6) {
+        throw new Exception('Invalid data!');
+    }
 
-//     if (!empty($check))
-//     {
-//         return $check->id;
-//     }
+    throwError($container, $request, [
+        'point' => v::floatVal()->notEmpty()->noWhitespace(),
+    ]);
+}
+die('die');
 
-//     return false;
-// }
+$data->point = (string) $data->point;
 
-// function getPosition($value)
-// {
-//     $position = DB::table('positions')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->where('parent_id', '!=', 0)->first();
+if (!empty($data->point)) {
+    $data->point = ltrim($data->point, 0);
 
-//     if (!empty($position))
-//     {
-//         return $position->title;
-//     }
+    $count = strlen($data->point);
 
-//     return false;
-// }
+    $dot = strpos($data->point, '.');
+    // echo $dot;die();
 
-// function checkLevel($value)
-// {
-//     $check = DB::table('level')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->first();
+    $dots = substr_count($data->point, '.');
 
-//     if (!empty($check))
-//     {
-//         return $check->id;
-//     }
+    if ($count == $dot + 1 || $dot === 0 || $dots > 1) {
+        throw new Exception('Invalid data!');
+    }
 
-//     return false;
-// }
+    if ($dot) {
+        $data->point = rtrim($data->point, 0);
 
-// function getLevel($value)
-// {
-//     $level = DB::table('level')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->first();
+        $count = strlen($data->point);
 
-//     if (!empty($level))
-//     {
-//         return $level->title;
-//     }
+        $dot = strpos($data->point, '.');
 
-//     return false;
-// }
+        if ($count == $dot + 1) {
+            $data->point = rtrim($data->point, '.');
+        }
+    }
 
-// function checkLevelPosition($value)
-// {
-//     $check = DB::table('level_positions')->where('position_id', $value)->where([ 'isdelete' => 0])->where('position_id', '!=', 0)->first();
+    echo $dot;die();
+}
 
-//     if (!empty($check))
-//     {
-//         return $check->position_id;
-//     }
+die('ok');
+$get_levels = DB::table('level')->where(['status' => 1, 'isdelete' => 0])->get();
 
-//     return false;
-// }
+$level_positions = DB::table('level_positions as lp')->where(['isdelete' => 0])->where('position_id', '!=', 0)->get();
 
-// foreach ($get_levels as $index => $v) 
-// {
-//     $levels[$v->id] = $v->title;
-// }
+$levels = [];
 
+function checkPosition($value)
+{
+    $check = DB::table('positions')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->where('parent_id', '!=', 0)->first();
 
+    if (!empty($check)) {
+        return $check->id;
+    }
 
+    return false;
+}
 
+function getPosition($value)
+{
+    $position = DB::table('positions')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->where('parent_id', '!=', 0)->first();
+
+    if (!empty($position)) {
+        return $position->title;
+    }
+
+    return false;
+}
+
+function checkLevel($value)
+{
+    $check = DB::table('level')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->first();
+
+    if (!empty($check)) {
+        return $check->id;
+    }
+
+    return false;
+}
+
+function getLevel($value)
+{
+    $level = DB::table('level')->where('id', $value)->where(['status' => 1, 'isdelete' => 0])->first();
+
+    if (!empty($level)) {
+        return $level->title;
+    }
+
+    return false;
+}
+
+function checkLevelPosition($value)
+{
+    $check = DB::table('level_positions')->where('position_id', $value)->where(['isdelete' => 0])->where('position_id', '!=', 0)->first();
+
+    if (!empty($check)) {
+        return $check->position_id;
+    }
+
+    return false;
+}
+
+foreach ($get_levels as $index => $v) {
+    $levels[$v->id] = $v->title;
+}
 
 // // *** update - quy đổi điểm
 // // 1. check position
 // // 2. check level
 // // 3. -> true ===> inserOrUpdate
-// // note: $point_positions là data trả về từ frontend 
+// // note: $point_positions là data trả về từ frontend
 // foreach($point_positions as $key => $title)
 // {
 //     if($key == getPosition($key)){
@@ -135,10 +174,10 @@ die($data->point);
 //                 DB::table('level_positions')->where('id', checkLevel($index))->where('id', checkPosition($key))->update([
 //                     'point' => $point_positions[$title][$index][$lable];
 //                 ]);
-//             }           
+//             }
 //         }
 //     } else {
-//         if (!empty(checkPosition($key))) {        
+//         if (!empty(checkPosition($key))) {
 //             $position_id = checkPosition($key);
 //             foreach($levels as $index => $level){
 //                 DB::table('level_positions')->insert([
@@ -150,13 +189,3 @@ die($data->point);
 //         }
 //     }
 // }
-
-
-
-
-
-
-
-
-
-
